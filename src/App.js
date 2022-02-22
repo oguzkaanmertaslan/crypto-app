ecf75f51dd13afce41c7551958f3ee5a56ab2e9e
oguzkaanmertaslan <kmertaslan66@gmail.com>
2022-02-22 23:01:09 +0300

    homepage exchanges and news added to footer

--- src/App.js
+++ src/App.js
@@ -31,8 +31,19 @@ const App = () => {
             </Switch>
           </div>
         </Layout>
+      <div className="footer" level={5} style={{color:"white", textAlign:"center"}}>
+          <Typography.Title>
+              Crypoverse <br/>
+              All rights reserved
+          </Typography.Title>
+          <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+
+          </Space>
       </div>
-      <div className="footer"></div>
+      </div>
     </div>
   );
 };
