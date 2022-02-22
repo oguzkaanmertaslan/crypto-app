d761fbf69e48be9d7494182f00dfccec61b5f8ed
oguzkaanmertaslan <kmertaslan66@gmail.com>
2022-02-22 22:50:35 +0300

    Homepage Cryptocurrecies Exchanges News CryptoDetails pages added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "./App";
-
+import "antd/dist/antd.css"
 ReactDOM.render(
   <Router>
     <App />
