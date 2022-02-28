e436a3a7851cff4c83453889354952c3bb99ce10
oguzkaanmertaslan <kmertaslan66@gmail.com>
2022-02-28 22:24:13 +0300

    cryptoDetails page written and linechart added in components

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -5,20 +5,24 @@ const cryptoApiHeaders = {
   "x-rapidapi-key": "3d9e2ab642mshe90d31ce0cf3528p1a68f9jsncf403ba600b4",
 };
 
-const baseUrl="https://coinranking1.p.rapidapi.com/";
+const baseUrl = "https://coinranking1.p.rapidapi.com/";
 
-const createRequest = (url)=>({url,headers:cryptoApiHeaders});
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
-export const cryptoApi=createApi({
-    reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({baseUrl}),
-    endpoints: (builder) => ({
-      getCryptos: builder.query({
-        query: (count) => createRequest(`/coins?limit=${count}`),
-      }),
-    })
-})
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({coinId,timeperiod}) => createRequest(`/coin/${coinId}/history/${timeperiod}`),
+    }),
+  }),
+});
 
-export const {
-    useGetCryptosQuery
-} = cryptoApi;
+export const { useGetCryptosQuery,useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
